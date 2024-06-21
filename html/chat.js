3097e85fad8cbd16fb2f74c40c2d743848b43328
Kyoungsu Park <kyopark2014@gmail.com>
2024-06-21 11:45:03 +0900

    update

--- html/chat.js
+++ html/chat.js
@@ -92,6 +92,7 @@ function connect(endpoint) {
 
     // message 
     eventSource.onmessage = function (event) {        
+        console.log('event: ', event);
         console.log('event: ', JSON.stringify(event));
         response = JSON.parse(event.data);
         console.log('response: ', response);
